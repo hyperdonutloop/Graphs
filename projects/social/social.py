0cb5e483cf3bb88689c7c545e238a320834be039
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-14 10:06:48 -0700

    completed social mvo

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,6 @@
+import random
+from util import Queue
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -44,9 +47,25 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        # Add Users
+        for i in range(1, num_users + 1):
+            self.add_user(i)
 
         # Create friendships
+        # to create N of random friendships
+        # create a list with all friendship combinations
+        # shuffle the list, then get the first N elements from the list
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+        
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,8 +76,35 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        # create a queue
+        q = Queue()
+        # enqueue a path to user_id
+        q.enqueue([user_id])
+        # create a dictionary for key value pairs
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        # while the queue is not empty..
+        while q.size() > 0:
+            # dequeue the first path
+            path = q.dequeue()
+            # grab the vertex from the end of the path
+            v = path[-1]
+            # check to see if it's been visited, and if not
+            if v not in visited:
+                if v is not user_id:
+                    # mark it as such
+                    visited[v] = path
+                # enqueue a path to all its neighbors
+                for neighbor in self.friendships[v]:
+                    if neighbor not in visited:
+                        # make a copy of the path
+                        copy = path.copy()
+                        # append the neighbor
+                        copy.append(neighbor)
+                        # enqueue the copy
+                        q.enqueue(copy)
+
+
+
         return visited
 
 
